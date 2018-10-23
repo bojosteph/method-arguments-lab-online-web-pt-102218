fdfed8bee0a76690603c4e6e3ec78bf91a183d76
Roberto Samia <bojosteph@gmail.com>
2018-10-23 12:28:21 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
-def introduction(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction("Robert", "Ruby")
